f7be420d227b54f919f7c243968d1b19df488eb4
Vinnicius Martins <VINNICIUSM@dpc.ad>
2021-08-24 13:13:20 -0300

    Criação da pagina Agendamentos só html

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from core.models import Evento
 
 # Create your views here.
+
+# def index(request):
+#     return redirect('/agenda/')
+
+def lista_eventos(request):
+    user = request.user
+    evento = Evento.objects.filter(usuario=user)
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
